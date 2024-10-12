f510d55ec0aa6b7e6f2ada982277959b28cdb076
Mahjoob97 <mahjoobalhaj4@gmail.com>
2024-10-12 19:46:19 +0300

    added the first banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import Hero from "./components/Hero";
 import Category from "./components/Category";
 import Category2 from "./components/Category2";
 import Service from "./components/Service";
+import Banner from "./components/Banner";
+import headphone from "./assets/headphone.png";
+
+const bannerData = {
+  discount: "30% off",
+  title: "Fine Smile",
+  date: "10 Jan to 29 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sales",
+  title4:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam autem maiores quos dignissimos natus iusto.",
+  bgColor: "#f42C37",
+};
 
 const App = () => {
   return (
@@ -13,6 +27,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Service />
+      <Banner data={bannerData} />
     </div>
   );
 };
